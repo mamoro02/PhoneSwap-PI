6b8f7ca4bebe0502d73230fd4d63d5ecee072ec6
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-05-14 09:31:48 +0200

    añadimos la clase Client con la funcionalidad de registro de un cliente en la web

--- signin.php
+++ signin.php
@@ -1,4 +1,34 @@
+<?php
 
+declare(strict_types=1);
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+if (count($_POST) > 0) {
+
+    try {
+
+        $client = new Client;
+
+        $dataClient = [
+            'userName' => $_POST['userName'],
+            'email' => $_POST['email'],
+            'password' => $_POST['password'],
+            'nameClient' => $_POST['nameClient'],
+            'sureName' => $_POST['sureName'],
+            'telephone' => $_POST['telephone']
+        ];
+
+        $client->insertClient($dataClient);
+
+        header("location:login.php"); //si el insert es correcto, nos redirigirá al login 
+
+    } catch (PDOException $e) {
+        echo "Se ha producido un error";
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +36,7 @@
     <title>SignIn - Phone Swap</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/login.css">
 </head>
 
@@ -20,8 +49,7 @@
                 <!-- Icono y nombre de la empresa -->
                 <div class="row col-3 offset-1">
                     <a class="navbar-brand" href="#">
-                        <img src="imagenes/icon/icono.png" width="33" style="transform: scale(2.0); margin-right: 5px;"
-                            class="d-inline-block align-top" alt="">
+                        <img src="imagenes/icon/icono.png" width="33" style="transform: scale(2.0); margin-right: 5px;" class="d-inline-block align-top" alt="">
                         <b>Phone Swap</b>
                     </a>
                 </div>
@@ -32,48 +60,46 @@
     </header>
     <main>
         <div class="container-fluid">
-            <div class="row" style=" height: 800px";>
+            <div class="row" style=" height: 800px" ;>
                 <div class="col-md-7"><img src="./imagenes/login.png"></div>
-                <div class="col-md-4 align-self-center">     
-            <form action="" method="post" class="signIn">
-                <h1 class="title">SIGN IN</h1>
-                <label>
-                    <i class="fa-solid fa-user"></i>
-                    <input placeholder="username" type="text" id="username" name="userName">
-                </label>
-                <label>
-                    <i class="fa-solid fa-user"></i>
-                    <input placeholder="email" type="email" id="email" name="email">
-                </label>
-                <label>
-                    <i class="fa-solid fa-lock"></i>
-                    <input placeholder="password" type="password" id="password" name="password">
-                </label>
-                <label>
-                    <i class="fa-solid fa-lock"></i>
-                    <input placeholder="nombre" type="text" id="nameClient" name="nameClient">
-                </label>
-                <label>
-                    <i class="fa-solid fa-lock"></i>
-                    <input placeholder="apellido" type="text" id="sureName" name="sureName">
-                </label>
-                <label>
-                    <i class="fa-solid fa-lock"></i>
-                    <input placeholder="telefono" type="tel" id="telephone" name="telephone">
-                </label>
-                <button id="button">Sign in</button>
-            </form>
-            
-            <script src="main.js"></script>
-        </div>
-        <div class="col-md-1"></div>
-        </div>
+                <div class="col-md-4 align-self-center">
+                    <form action="" method="post" class="signIn">
+                        <h1 class="title">SIGN IN</h1>
+                        <label>
+                            <i class="fa-solid fa-user"></i>
+                            <input placeholder="username" type="text" id="username" name="userName">
+                        </label>
+                        <label>
+                            <i class="fa-solid fa-user"></i>
+                            <input placeholder="email" type="email" id="email" name="email">
+                        </label>
+                        <label>
+                            <i class="fa-solid fa-lock"></i>
+                            <input placeholder="password" type="password" id="password" name="password">
+                        </label>
+                        <label>
+                            <i class="fa-solid fa-lock"></i>
+                            <input placeholder="nombre" type="text" id="nameClient" name="nameClient">
+                        </label>
+                        <label>
+                            <i class="fa-solid fa-lock"></i>
+                            <input placeholder="apellido" type="text" id="sureName" name="sureName">
+                        </label>
+                        <label>
+                            <i class="fa-solid fa-lock"></i>
+                            <input placeholder="telefono" type="tel" id="telephone" name="telephone">
+                        </label>
+                        <button id="button">Sign in</button>
+                    </form>
+
+                    <script src="main.js"></script>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
         </div>
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
