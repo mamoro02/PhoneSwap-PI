fc5f4c5cfde15b2f393103da7647f74ffe8d371f
sealmo <sealmo@floridauniversitaria.es>
2023-05-16 09:44:13 +0200

    item

--- Item.php
+++ Item.php
@@ -25,7 +25,7 @@
 
                 <!-- Icono y nombre de la empresa -->
                 <div class="row col-3 offset-1">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="index.php">
                         <img src="imagenes/icon/icono.png" width="33" style="transform: scale(2.0)"
                             class="d-inline-block align-top" alt="">
                         <b>Phone Swap</b>
